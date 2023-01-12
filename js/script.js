96313358d47470f59ce003dfa1100d5960785fd3
stacy_r <stacyrileyca@gmail.com>
2023-01-12 15:00:53 -0500

    Created function to filter data and addeventlisteners

--- js/script.js
+++ js/script.js
@@ -9,8 +9,35 @@ const cities = [];
 //blob.json() returns another promise:
 fetch(endpoint)
     .then(blob => blob.json(blob))
-    .then(data => cities.push(...data)) //"..." spread into function or this push method:
-    
+ //"..." spread used with .push() the items into the array as individual arguments
+    .then(data => cities.push(...data)) 
 
-    console.log(cities)
+// When user inputs data, we need to filter it down to only return a match:
+
+function findMatches(wordToMatch, cities){
+    return cities.filter(place => {
+//Here we need to figure out if the city or state match what was searched:
+//we need to put a variable into a regular expression: g=global i =case insensitive 
+const regex = new RegExp(wordToMatch, 'gi')        
+return place.city.match(regex) || place.state.match(regex)
+    });
+}
+
+// Display function
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities)
+    // console.log(matchArray)
+
+
+    //This console.log displays the 'change' and 'keyup' entries in the console
+    // console.log(this.value)
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+//Listen for a change inside the input field and when present, run function FindMatches
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+    // console.log(cities)
 // console.log(prom);
